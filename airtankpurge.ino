d48b6765124b4b134c2b4949b2e147ea2d8038da
rccipriani <rccipriani@gmail.com>
2022-02-17 16:21:58 -0500

    Add files via upload

--- airtankpurge.ino
+++ airtankpurge.ino
@@ -1,18 +1,26 @@
-//will activate compressor purge solenoid for 10 seconds every 24 hours
-//button will purge on demand
-//button may need debounce logic
+//v1.0.1
 
-unsigned long interval = 86400000; //24 hours
-int purgeTime = 10000; //10 seconds
+//will activate compressor purge solenoid for ~10 seconds every 24 hours
+//button will purge on demand
+//using an ASCO 1/4" 8262H022-12/DC solenoid valve with "safety shut-off" so valve will stay open about twice as long as relay is energized
+//I assume this feature is to prevent "water hammer" when used in liquid applications (valve doesn't close instantly)
+
+//conversion factor for hours
+static unsigned long int hoursToMs = 3600000;
+
+unsigned long interval = 24 * hoursToMs;
+int purgeTime = 5000; //5 seconds
 unsigned long previousMillis = 0;
 
 void setup() {
+  interrupts(); //should be on by default
+
   pinMode(0, OUTPUT); //RELAY CONTROL
   pinMode(1, INPUT_PULLUP);  //MANUAL BUTTON, PRESSED=LOW
 
   //test
   digitalWrite(0, HIGH);
-  delay(2000); //2 seconds
+  delay(1000); //1 seconds
 
   //reset
   digitalWrite(0, LOW);
@@ -21,7 +29,7 @@ void setup() {
 
 void loop()
 {
-  unsigned long currentMillis = millis(); //millis() returns the number of milliseconds the Arduino has been running. Resets to 0 after approx 50 days.
+  unsigned long currentMillis = millis(); //millis() returns the number of milliseconds the Arduino has been running. Loops back to 0 after about 50 days
 
   if (currentMillis - previousMillis > interval) {
     previousMillis = currentMillis;
@@ -31,7 +39,7 @@ void loop()
   if (digitalRead(1) == LOW) {
     purge();
     previousMillis = currentMillis; //on manual purge, start the 24 hour timer from "now"
- }
+  }
 
 }
 
