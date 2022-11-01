373b866abf7b66d0f999d6f4d80519d57f6e02f9
rccipriani <rccipriani@gmail.com>
2022-11-01 13:10:28 -0400

    Add files via upload

--- airtankpurge.ino
+++ airtankpurge.ino
@@ -1,20 +1,18 @@
-//v1.0.2
+//v1.0.4
 
-//will activate compressor purge solenoid for ~10 seconds every 24 hours
+//will activate compressor purge solenoid for ~5 seconds every 24 hours
 //button will purge on demand
-//using an ASCO 1/4" 8262H022-12/DC solenoid valve with "safety shut-off" so valve will stay open about twice as long as relay is energized
-//I assume this feature is to prevent "water hammer" when used in liquid applications (valve doesn't close instantly)
+//using an ASCO 1/4" 8262H022-12/DC solenoid valve
 
-//conversion factor for hours
-static unsigned long int hoursToMs = 3600000;
-
-unsigned long interval = 24 * hoursToMs;
+static unsigned long int hoursToMs = 3600000;  //conversion factor for hours to milliseconds
+unsigned long interval = 24 * hoursToMs; //interval between automatic purges in hours
 int purgeTime = 5000; //5 seconds
 int loopDelay = 250; //250 ms
-unsigned long previousMillis = 0;
+unsigned long previousMillis = 0; //millis() count for last purge
+int switchRead = 0; //flag for first switch read
 
 void setup() {
-  interrupts(); //should be on by default
+  interrupts(); //should be on by default, needed for millis()
 
   pinMode(0, OUTPUT); //RELAY CONTROL
   pinMode(1, INPUT_PULLUP);  //MANUAL BUTTON, PRESSED=LOW
@@ -38,12 +36,16 @@ void loop()
   }
 
   if (digitalRead(1) == LOW) {
+    switchRead = 1;
     purge();
     previousMillis = currentMillis; //on manual purge, start the 24 hour timer from "now"
   }
 
   //adding a delay here, it seemed the loop was running so frequently that it was picking up the switch press multiple times
-  delay(loopDelay);
+  if (switchRead == 1) {
+    delay(loopDelay);
+    switchRead = 0;
+  }
 }
 
 
@@ -52,4 +54,6 @@ void purge()
   digitalWrite(0, HIGH);
   delay(purgeTime);
   digitalWrite(0, LOW);
+  //adding a delay   
+  delay(5000);
 }
